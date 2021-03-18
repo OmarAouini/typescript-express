9ff67917b54c49603812f0e550a4528cf3e22941
DESKTOP-DEMD92I\Aouini <aouini.omar93@gmail.com>
2021-03-18 13:53:43 +0100

    init commit projext structure

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+import logger from "./utils/logger"
+const app = express();
+dotenv.config();
+
+//env variables
+const port = process.env.PORT;
+
+//middlewares
+initMiddleWares(app);
+
+//routes
+app.get("/", (req, res) =>{
+    res.status(200).send("hello")
+});
+
+//404 handler
+app.use((req, res) => {
+    logger.info("entering 404 handler...")
+    return res.status(404).send("404 not found!");
+});
+
+//server start
+app.listen(port, () => {
+    logger.info("starting server application...");
+    logger.info(`app is listening on http://localhost:${port}`);
+});
+
+
+function initMiddleWares(app :express.Application): void {
+    logger.info("init middlewares...")
+    app.use(morgan("dev"));
+    app.use(cors())
+    app.use(helmet())
+}
